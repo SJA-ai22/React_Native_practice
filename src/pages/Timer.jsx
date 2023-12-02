4bcc6c01172adf536b2a8f3f9b309df5dfca7a4e
SJA-ai22 <jashin0414@khu.ac.kr>
2023-12-02 11:33:04 +0900

    base timer set 4 hours

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -33,10 +33,10 @@ const useCounter = (initialValue, ms) => {
 
 export default function SetTimer() {
   // 시, 분, 초를 state로 저장
-  const [currentHours, setCurrentHours] = useState(2); // start with 2 hours
+  const [currentHours, setCurrentHours] = useState(4); // start with 4 hours
   const [currentMinutes, setCurrentMinutes] = useState(0);
   const [currentSeconds, setCurrentSeconds] = useState(0);
-  const { count, start, stop, reset } = useCounter(2 * 3600, 1000); // 2 hours in seconds
+  const { count, start, stop, reset } = useCounter(4 * 3600, 1000); // 4 hours in seconds
 
   // 타이머 기능
   const timer = () => {
